a63cddd2aa07ff7eb4cf1a4711c40429410e6545
fernanda <guerreirosalomaofernanda@gmail.com>
2024-10-23 17:52:06 -0400

    adição dos cookies - sem estilização

--- cookies.js
+++ cookies.js
@@ -1,46 +1,33 @@
-// Função para criar um cookie
-function setCookie(nome, valor, dias) {
-  const data = new Date();
-  data.setTime(data.getTime() + dias * 24 * 60 * 60 * 1000);
-  const expira = "expires=" + data.toUTCString();
-  document.cookie = nome + "=" + valor + ";" + expira + ";path=/";
-}
+var msgCookies = document.getElementById("cookies-msg");
 
-// Função para ler um cookie
-function getCookie(nome) {
-  const nomeIgual = nome + "=";
-  const decodificado = decodeURIComponent(document.cookie);
-  const ca = decodificado.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(nomeIgual) == 0) {
-      return c.substring(nomeIgual.length, c.length);
-    }
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
   }
-  return "";
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
-// Função para apagar um cookie
-function apagarCookie(nome) {
-  document.cookie = nome + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-// Exemplo de uso
-function exemploCookies() {
-  // Criar um cookie
-  setCookie("username", "JohnDoe", 30);
-
-  // Ler um cookie
-  const user = getCookie("username");
-  if (user != "") {
-    alert("Bem-vindo de volta, " + user);
-  } else {
-    alert("Bem-vindo, novo usuário!");
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
+  return null;
+}
 
-  // Apagar um cookie
-  // apagarCookie("username"); // Descomente esta linha para apagar o cookie
+function aceitar() {
+  setCookie("lgpd", "sim", 365);
+  msgCookies.classList.remove("mostrar");
+}
+
+if (getCookie("lgpd") == 'sim') {
+  msgCookies.classList.remove("mostrar");
+} else {
+  msgCookies.classList.add("mostrar");
 }
